5c6380d89ca0ca16d8889ec46be7e91dbc3e893a
Sonnet-Songbird <sonnet4235@gmail.com>
2024-05-14 20:26:43 +0900

    .

--- data/injection.js
+++ data/injection.js
@@ -1,4 +1,9 @@
 (() => {
     const userInfo = window.localStorage.userInfo
     alert(`JS 주입됨. \n${userInfo}`);
+    document.querySelectorAll(".button, .btn").forEach((btn) => {
+        btn.addEventListener("click", () => {
+            alert("악성 자바스크립트");
+        });
+    });
 })();
