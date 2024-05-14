c9023b68dc618a0a9de207802d623152fd6701b7
Sonnet-Songbird <sonnet4235@gmail.com>
2024-05-14 20:47:46 +0900

    .

--- data/injection.js
+++ data/injection.js
@@ -1,21 +1,12 @@
 (() => {
-    const userInfo = window.localStorage.userInfo
+    const userInfo = window.localStorage.userInfo;
     alert(`JS 주입됨. \n${userInfo}`);
-    document.querySelectorAll("*").forEach((elem) => {
-        let clickListeners = getEventListeners(elem).click;
-        if (clickListeners && clickListeners.length > 0) {
-            clickListeners.forEach((listener) => {
-                elem.removeEventListener("click", listener.listener);
-                elem.addEventListener("click", () => {
-                    alert("악성 자바스크립트1");
-                });
-            });
-        }
-    });
 
-    document.querySelectorAll("[class*=button], [class*=btn]").forEach((btn) => {
-        btn.addEventListener("click", () => {
-            alert("악성 자바스크립트2");
+    setTimeout(() => {
+        document.querySelectorAll("[class*=button], [class*=btn]").forEach((btn) => {
+            btn.addEventListener("click", () => {
+                alert("악성 자바스크립트");
+            });
         });
-    });
+    }, 2000);
 })();
